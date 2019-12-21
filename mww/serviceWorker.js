4cb53a5a4c9ba1a18f1cb914891d6b7634796b93
Le Duy Quang <leduyquang753@gmail.com>
2019-12-21 20:08:45 +0700

    Cache the notification sound.

--- mww/serviceWorker.js
+++ mww/serviceWorker.js
@@ -1,11 +1,16 @@
 ﻿self.addEventListener('install', (event) => {
-  self.skipWaiting();
+	event.waitUntil(caches.open('mww-cache').then(cache => {
+		return cache.add('New_change.wav');
+	}));
 });
 
 self.addEventListener('activate', (event) => {
-  return self.clients.claim();
+	return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+	event.respondWith(caches.match(event.request).then(response => {
+		if (response) return response;
+		return fetch(event.request);
+	}));
 });
